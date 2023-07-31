bb92e5503e0633d39e7847e8f7f83523db4828f2
shaivy <shaivygowda@gmail.com>
2023-07-31 13:31:36 +0530

    number vowels added to dictionary

--- tst.py
+++ tst.py
@@ -1,6 +1,8 @@
 l=['ABA','PYT','XYZ','MOM','NAN','HANNAH']
 d={}
 d1={}
+d2={}
+c=0
 for i in l:
     print(i)
 for i in l:
@@ -12,3 +14,10 @@ for i in l:
     print(i,len(i))
 for i in l:
     d1[i]=len(i)
+for i in l:
+    c=0
+    for j in  range(len(i)):
+        if i[j] in ['AEIOU']:
+            c+=1
+    d[i]=c
+print(d)
